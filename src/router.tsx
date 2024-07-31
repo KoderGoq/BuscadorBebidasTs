7b0156ed3cbdb56e6945ad4f4cd90833b231add3
Fernando Aguilar <100661177+KoderGoq@users.noreply.github.com>
2024-07-30 19:31:07 -0600

    Add Performance in IndexPage

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,8 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import IndexPage from './views/IndexPage';
 import Layout from './layouts/Layout';
 
-
+const IndexPage = lazy(() => import('./views/IndexPage'));
 const FavoritesPage = lazy(() => import('./views/FavoritesPage'));
 
 const AppRouter = () => {
@@ -11,7 +10,11 @@ const AppRouter = () => {
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path='/' element={<IndexPage />} index />
+          <Route path='/' element={
+            <Suspense fallback='Cargando..'>
+              <IndexPage />
+            </Suspense>
+          } index />
           <Route path='/favoritos' element={
             <Suspense fallback='Cargando..'>
               <FavoritesPage />
